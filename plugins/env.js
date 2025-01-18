aab9e5948bc5d38246871f97fee2eb127973a610
Mr Legend <163214741+legend5454@users.noreply.github.com>
2025-01-18 17:41:09 +0500

    Update env.js

--- plugins/env.js
+++ plugins/env.js
@@ -52,7 +52,7 @@ async (conn, mek, m, { from, quoted, reply }) => {
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
-                    isForwarded: true,
+                    isForwarded: false,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363328183826270@newsletter',
                         newsletterName: "♛︎ᴍʀ ʟᴇɢᴇɴᴅ࿐",
